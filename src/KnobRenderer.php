3d9a4cc0f43cd8dda7f7785950bc970b81e41715
Sullivan SENECHAL <sullivan@nexylan.com>
2015-10-15 14:10:26 +0200

    Code init

---
+++ src/KnobRenderer.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace SLLH\Knob;
+
+/**
+ * @author Sullivan Senechal <soullivaneuh@gmail.com>
+ */
+class KnobRenderer
+{
+}
---
+++ tests/KnobRendererTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace SLLH\Knob\Tests;
+
+use SLLH\Knob\KnobRenderer;
+
+/**
+ * @author Sullivan Senechal <soullivaneuh@gmail.com>
+ */
+class KnobRendererTest extends \PHPUnit_Framework_TestCase
+{
+    public function test()
+    {
+        $renderer = new KnobRenderer();
+    }
+}
