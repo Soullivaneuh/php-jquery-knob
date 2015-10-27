4d8ff388ff291fb3ede9c7c02fbe2005789d3340
Sullivan SENECHAL <soullivaneuh@gmail.com>
2015-10-27 22:31:33 +0100

    Introduce options-resolver

--- src/KnobRenderer.php
+++ src/KnobRenderer.php
@@ -2,9 +2,22 @@
 
 namespace SLLH\Knob;
 
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
 /**
  * @author Sullivan Senechal <soullivaneuh@gmail.com>
  */
-class KnobRenderer
+final class KnobRenderer
 {
+    public function renderInput($options)
+    {
+        $resolver = new OptionsResolver();
+        $this->configureOptions($resolver);
+
+        $options = $resolver->resolve($options);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+    }
 }
